76a9096289ef6f703bcb74fd53cd989561787fac
Preston Elliott <preston.elliott24@gmail.com>
2019-04-18 21:54:01 +0000

    Automatically backed up by Learn

--- guessing_cli.rb
+++ guessing_cli.rb
@@ -4,7 +4,7 @@ def run_guessing_game
   while guess
     puts "Guess a number between 1 and 6."
     guess = gets.downcase.chomp
-    random_number = rand(1..6)
+    random_number = rand(1..6).to_s
     if random_number == guess
       puts "You guessed the correct number!"
     elsif guess == "exit"
